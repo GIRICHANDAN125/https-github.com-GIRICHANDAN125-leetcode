500962f4b797386fc5dd7b0f86e2794c6fd64bd1
GIRI CHANDAN <chandugiri125@gmail.com>
2026-08-05 22:05:08 +0530

    Time: 0 ms (100%), Space: 11.7 MB (12.1%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int l=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                swap(nums[l],nums[i]);
+                l++;
+            }
+        }
+
+        int r=nums.size()-1;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(nums[i]==2){
+                swap(nums[r],nums[i]);
+                r--;
+            }
+        }
+   
+
+    }
+};
